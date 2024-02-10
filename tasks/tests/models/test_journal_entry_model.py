accb8eae8f99070122b7881aa9efe62611d31ac7
Ishika Arora <ishika.arora@kcl.ac.uk>
2024-02-10 22:52:59 +0000

    Write tests for changes in journal entry model

--- tasks/tests/models/test_journal_entry_model.py
+++ tasks/tests/models/test_journal_entry_model.py
@@ -18,6 +18,16 @@ class JournalEntryModel(TestCase):
             deleted=False
         )
 
+    def test_journal_entry_creation_with_mood(self):
+        """Test creating a journal entry with a specific mood"""
+        entry = JournalEntry.objects.create(
+            user=self.user,
+            title='Mood Entry',
+            text='Testing moods',
+            mood=5  # Very happy mood
+        )
+        self.assertEqual(entry.mood, 5)
+
     def test_delete_entry(self):
         self.entry.delete_entry()
         updated_entry = JournalEntry.objects.get(pk=self.entry.pk)
@@ -29,3 +39,12 @@ class JournalEntryModel(TestCase):
         self.entry.recover_entry()
         updated_entry = JournalEntry.objects.get(pk=self.entry.pk)
         self.assertFalse(updated_entry.deleted)
+    
+    def test_default_mood_value(self):
+        """Test that the default mood value is applied correctly"""
+        default_mood_entry = JournalEntry.objects.create(
+            user=self.user,
+            title='Default Mood Entry',
+            text='Text without specifying mood'
+        )
+        self.assertEqual(default_mood_entry.mood, 3)
