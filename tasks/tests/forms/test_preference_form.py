8598ed4c723f43ffc5e7ee78be0ba7ad9df7f0ec
Hannah Ishimwe <hannah.ishimwe@kcl.ac.uk>
2024-02-18 15:50:46 +0000

    Write form test

--- tasks/tests/forms/test_preference_form.py
+++ tasks/tests/forms/test_preference_form.py
@@ -4,6 +4,7 @@ from django import forms
 from django.test import TestCase
 from tasks.forms import UserPreferenceForm
 from tasks.models import User, UserPreferences
+import datetime
 
 class SignUpFormTestCase(TestCase):
     """Unit tests of the sign up form."""
@@ -15,7 +16,6 @@ class SignUpFormTestCase(TestCase):
     def setUp(self):
         self.form_input = {
             'journal_time': '17:30:00',
-            'number_of_times_to_journal': '3',
             'monday': 'True',
             'tuesday': 'True',
             'wednesday': 'False',
@@ -28,17 +28,13 @@ class SignUpFormTestCase(TestCase):
         self.days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
         
     
-    def set_up(self):
-        pass
-
-    def test_valid_sign_up_form(self):
+    def test_valid_preference_up_form(self):
         form = UserPreferenceForm(data=self.form_input)
         self.assertTrue(form.is_valid())
 
     def test_form_has_necessary_fields(self):
         form = UserPreferenceForm()
         self.assertIn('journal_time', form.fields)
-        self.assertIn('number_of_times_to_journal', form.fields)
         for day in self.days:
             self.assertIn(day, form.fields)
             day_field = form.fields[day]
@@ -46,43 +42,22 @@ class SignUpFormTestCase(TestCase):
             day_widget = day_field.widget
             self.assertTrue(isinstance(day_widget, forms.CheckboxInput))    
 
-    def test_form_uses_model_validation(self):
-        self.form_input['username'] = 'badusername'
-        form = UserPreferenceForm(data=self.form_input)
-        self.assertFalse(form.is_valid())
 
-    def test_password_must_contain_uppercase_character(self):
-        self.form_input['new_password'] = 'password123'
-        self.form_input['password_confirmation'] = 'password123'
-        form = UserPreferenceForm(data=self.form_input)
-        self.assertFalse(form.is_valid())
-
-    def test_password_must_contain_lowercase_character(self):
-        self.form_input['new_password'] = 'PASSWORD123'
-        self.form_input['password_confirmation'] = 'PASSWORD123'
-        form = UserPreferenceForm(data=self.form_input)
-        self.assertFalse(form.is_valid())
-
-    def test_password_must_contain_number(self):
-        self.form_input['new_password'] = 'PasswordABC'
-        self.form_input['password_confirmation'] = 'PasswordABC'
-        form = UserPreferenceForm(data=self.form_input)
-        self.assertFalse(form.is_valid())
-
-    def test_new_password_and_password_confirmation_are_identical(self):
-        self.form_input['password_confirmation'] = 'WrongPassword123'
+    def test_journal_time_must_be_time(self):
+        self.form_input['journal_time'] = '17'
         form = UserPreferenceForm(data=self.form_input)
         self.assertFalse(form.is_valid())
 
     def test_form_must_save_correctly(self):
         form = UserPreferenceForm(data=self.form_input)
-        before_count = User.objects.count()
+        form.instance.user = self.user
+        before_count = UserPreferences.objects.count()
         form.save()
-        after_count = User.objects.count()
+        after_count = UserPreferences.objects.count()
         self.assertEqual(after_count, before_count+1)
-        user = User.objects.get(username='@janedoe')
-        self.assertEqual(user.first_name, 'Jane')
-        self.assertEqual(user.last_name, 'Doe')
-        self.assertEqual(user.email, 'janedoe@example.org')
-        is_password_correct = check_password('Password123', user.password)
-        self.assertTrue(is_password_correct)
+        pref = UserPreferences.objects.get(user=1)
+        self.assertEqual(pref.journal_time, datetime.time(17, 30))
+        for day in ['monday', 'tuesday', 'thursday']:
+            self.assertTrue(getattr(pref, day))
+        for day in ['wednesday', 'friday', 'saturday', 'sunday']:
+            self.assertFalse(getattr(pref, day))
