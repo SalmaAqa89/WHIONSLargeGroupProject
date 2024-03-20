bfe991fdecb0c1fa5091f1dde748cd7291560c9a
Hannah Ishimwe <hannah.ishimwe@kcl.ac.uk>
2024-03-20 01:39:36 +0000

    Attempt UI

---
+++ tasks/context_processor.py
@@ -0,0 +1,18 @@
+from django.utils import timezone
+from datetime import timedelta
+from .models import JournalEntry
+
+def add_journal_streak(request):
+    now = timezone.now()
+    all_entries = JournalEntry.objects.filter(user=request.user)
+    dates_journaled = {entry.created_at.date() for entry in all_entries}
+    streak = 0
+    date = now.date()
+    while True:
+        if date in dates_journaled:
+            streak += 1
+        elif date != now.date():
+            break
+        date -= timedelta(days=1)
+
+    return {'journal_streak': streak}
