3efaadc15caa02b3a65dacd04f5e46513c629e00
Hannah Ishimwe <hannah.ishimwe@kcl.ac.uk>
2024-03-22 21:15:24 +0000

    Change order in which entries appear

--- tasks/views/PageViews.py
+++ tasks/views/PageViews.py
@@ -41,8 +41,8 @@ def dashboard(request):
 def journal_log(request):
     start_date = timezone.now() - timedelta(days=30)
     end_date = timezone.now()
-    journal_entries_last_thirty_days = JournalEntry.objects.filter(user=request.user, created_at__range=(start_date, end_date))
-    return render(request, 'pages/journal_log.html', {'journal_entries' : JournalEntry.objects.filter(user=request.user),
+    journal_entries_last_thirty_days = JournalEntry.objects.filter(user=request.user, created_at__range=(start_date, end_date)).order_by('-created_at')
+    return render(request, 'pages/journal_log.html', {'journal_entries' : JournalEntry.objects.filter(user=request.user).order_by('-created_at'),
                                                       'journal_entries_last_thirty_days' : journal_entries_last_thirty_days,})
 
 @login_required
