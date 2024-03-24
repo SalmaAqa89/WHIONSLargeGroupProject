6336eff293f7a17f9605d3ffbd4bd11108369f3d
Ishika Arora <ishika.arora@kcl.ac.uk>
2024-03-24 13:37:50 +0000

    Alter settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -169,6 +169,7 @@ EMAIL_USE_TLS = True
 # Email account credentials
 EMAIL_HOST_USER = 'WHIONS@outlook.com'
 EMAIL_HOST_PASSWORD = 'LGSEGproject2024!'
+EMAIL_FROM = 'WHIONS@outlook.com'
 DEFAULT_FROM_EMAIL = 'WHIONS@outlook.com'
 
 
