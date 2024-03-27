56297f1bb8fa17a52640f197d145ed4ec8f1f063
Wanzhen Wang <k20069280@kcl.ac.uk>
2024-03-27 11:46:05 +0000

    see more test done

---
+++ tasks/tests/test_seemore.py
@@ -0,0 +1,21 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+class MySeleniumTests(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver(service=Service(ChromeDriverManager().install()))
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_show_hide_text(self):
+        self.selenium.get(f'{self.live_server_url}/path/to/your/page')
+        # Here you'd add your Selenium commands to interact with the page
+        # and assert conditions, similar to the examples provided earlier.
